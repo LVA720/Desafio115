c3951b8f84aa6be2a4aa568576291dbc3f62fd26
LVA720 <158532152+LVA720@users.noreply.github.com>
2025-10-16 23:56:07 +0900

    if main

--- src/main.py
+++ src/main.py
@@ -1,36 +1,42 @@
 from meupacote import menu, pessoas
 
-while True:
-    menu("MENU PRINCIPAL")
-    print("\033[90mCtrl + C finaliza o programa\033[m")
+
+def main():
     while True:
-        try:
-            opc = str(input("\033[32m Sua Opcao: \033[m"))
-            if opc.isnumeric():
-                opc = int(opc)
-                if 1 <= opc <= 5:
-                    break
+        menu("MENU PRINCIPAL")
+        print("\033[90mCtrl + C finaliza o programa\033[m")
+        while True:
+            try:
+                opc = str(input("\033[32m Sua Opcao: \033[m"))
+                if opc.isnumeric():
+                    opc = int(opc)
+                    if 1 <= opc <= 5:
+                        break
+                    else:
+                        print("\033[0;31mERRO! Opcao invalida.\033[m")
                 else:
                     print("\033[0;31mERRO! Opcao invalida.\033[m")
-            else:
-                print("\033[0;31mERRO! Opcao invalida.\033[m")
-        except KeyboardInterrupt:
-            opc = 5
+            except KeyboardInterrupt:
+                opc = 5
+                break
+
+        if opc == 1:
+            menu("PESSOAS CADASTRADAS", False)
+            pessoas(opc)
+        if opc == 2:
+            menu("NOVO CADASTRO", False)
+            pessoas(opc)
+        if opc == 3:
+            menu("EDITAR CADASTRO", False)
+            pessoas(opc)
+        if opc == 4:
+            menu("DELETAR CADASTRO", False)
+            pessoas(opc)
+        if opc == 5:
             break
 
-    if opc == 1:
-        menu("PESSOAS CADASTRADAS", False)
-        pessoas(opc)
-    if opc == 2:
-        menu("NOVO CADASTRO", False)
-        pessoas(opc)
-    if opc == 3:
-        menu("EDITAR CADASTRO", False)
-        pessoas(opc)
-    if opc == 4:
-        menu("DELETAR CADASTRO", False)
-        pessoas(opc)
-    if opc == 5:
-        break
-
 print("PROGRAMA FINALIZADO")
+
+if __name__ == "__main__":
+    main()
+    
