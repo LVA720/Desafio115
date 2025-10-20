a6ca4d41042e77153318521387acb147d2f0bec3
Bruno Oshiro <75125546+BrunoOshiro27@users.noreply.github.com>
2025-10-21 02:54:11 +0900

    Refactor command handling and input validation
    Refactor command handling into a separate function and simplify option validation.

--- src/main.py
+++ src/main.py
@@ -4,39 +4,35 @@ from meupacote import humano
 pessoa = humano("dados.json")
 
 def main():
+    def handle_command(command):
+        match command:
+            case 1:
+                menu("PESSOAS CADASTRADAS", False)
+                pessoa.cadastro()
+            case 2:
+                menu("NOVO CADASTRO", False)
+                pessoa.cadastrar()
+            case 3:
+                menu("EDITAR CADASTRO", False)
+                pessoa.editar()
+            case 4:
+                menu("DELETAR CADASTRO", False)
+                pessoa.apagar()
+            case 5:
+                print("\nPROGRAMA FINALIZADO")
+            case _:  # Default case, acts like 'else'
+                print("\033[0;31mERRO! Opcao invalida.\033[m")
     while True:
         menu("MENU PRINCIPAL")
         print("\033[90mCtrl + C finaliza o programa\033[m")
         while True:
             try:
                 opc = str(input("Sua Opcao: "))
-                if opc.isnumeric():
-                    opc = int(opc)
-                    if 1 <= opc <= 5:
-                        break
-                    else:
-                        print("\033[0;31mERRO! Opcao invalida.\033[m")
-                else:
-                    print("\033[0;31mERRO! Opcao invalida.\033[m")
+                opc = int(opc) if opc.isdigit() else print("\033[0;31mERRO! Opcao invalida.\033[m")
+                handle_command(opc)
             except KeyboardInterrupt:
-                opc = 5
+                handle_command(5)
                 break
-
-        if opc == 1:
-            menu("PESSOAS CADASTRADAS", False)
-            pessoa.cadastro()
-        if opc == 2:
-            menu("NOVO CADASTRO", False)
-            pessoa.cadastrar()
-        if opc == 3:
-            menu("EDITAR CADASTRO", False)
-            pessoa.editar()
-        if opc == 4:
-            menu("DELETAR CADASTRO", False)
-            pessoa.apagar()
-        if opc == 5:
-            print("\nPROGRAMA FINALIZADO")
-            break
 
 if __name__ == "__main__":
     main()
